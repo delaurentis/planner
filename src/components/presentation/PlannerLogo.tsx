3bffedd1a045aa2412f0898787aacd9a47165e5f
Pete DeLaurentis <pete@RENG-PDeLaurentis-DXF4HG4Y7J.local>
2024-11-07 16:15:21 -0800

    Fixed deprecation warning with default props

--- src/components/presentation/PlannerLogo.tsx
+++ src/components/presentation/PlannerLogo.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
 interface PlannerLogoProps {
-  width?: number;
   height?: number;
 }
 
-const defaultProps = {
-  height: 28,
-  isInverted: false
-};
-
-const PlannerLogo: React.FC<PlannerLogoProps> = (props) => {
-  const width = Math.round(267 / 40 * (props.height || 28));
+const PlannerLogo: React.FC<PlannerLogoProps> = ({ height = 28, ...props }) => {
+  const width = Math.round(267 / 40 * height);
   return (
     <span>Planner</span>
   );
